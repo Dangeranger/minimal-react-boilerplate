abac26e2ff229e2f70a4c498ee0103345b210b45
Joshua Burke <burke.joshua.james@gmail.com>
2018-05-22 15:35:14 -0400

    Require React and configure webpack to hot reload

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
+const webpack = require('webpack');
+
 module.exports = {
   entry: [
+    'react-hot-loader/patch',
     './src/index.js'
   ],
   module: {
@@ -16,7 +19,11 @@ module.exports = {
     publicPath: '/',
     filename: 'bundle.js'
   },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
   devServer: {
-    contentBase: './dist'
+    contentBase: './dist',
+    hot: true
   }
 };
